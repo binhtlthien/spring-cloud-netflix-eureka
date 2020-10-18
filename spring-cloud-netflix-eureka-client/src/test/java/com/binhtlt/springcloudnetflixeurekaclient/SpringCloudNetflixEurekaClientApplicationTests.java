7aa40368bc49e62e712ef49b5807457f4027d1b9
binhtlt <binhtlt@vng.com.vn>
2020-10-18 20:25:10 +0700

    Add Spring cloud Netflix Eureka Client demo

---
+++ spring-cloud-netflix-eureka-client/src/test/java/com/binhtlt/springcloudnetflixeurekaclient/SpringCloudNetflixEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhtlt.springcloudnetflixeurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNetflixEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
